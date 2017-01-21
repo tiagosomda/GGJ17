0bded2a67d082cd9177325de025499778a861f96
tiagosomda <tsomda@gmail.com>
2017-01-21 05:55:05 -0800

    Added basic movement and wall blocking

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction { up, down, left, right };
+
+public class PlayerMovement : MonoBehaviour {
+
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+
+    public float speed;
+
+    public Map map;
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyUp(moveLeft))
+        {
+            Move(Direction.left);
+        }
+
+        if (Input.GetKeyUp(moveRight))
+        {
+            Move(Direction.right);
+        }
+
+        if (Input.GetKeyUp(moveUp))
+        {
+            Move(Direction.up);
+        }
+
+        if (Input.GetKeyUp(moveDown))
+        {
+            Move(Direction.down);
+        }
+    }
+
+    public void Move(Direction dir)
+    {
+        var pos = transform.position;
+        var rot = transform.rotation;
+
+        switch (dir)
+        {
+            case Direction.up:
+                pos.y += 1;
+                rot = Quaternion.Euler(0, 0, 0);
+                break;
+            case Direction.down:
+                pos.y -= 1;
+                rot = Quaternion.Euler(0, 0, 180);
+                break;
+            case Direction.left:
+                rot = Quaternion.Euler(0, 0, 90);
+                pos.x -= 1;
+                break;
+            case Direction.right:
+                pos.x += 1;
+                rot = Quaternion.Euler(0, 0, 270);
+                break;
+        }
+
+        if (map.CanMove(transform.position, dir))
+        {
+            transform.position = pos;
+        }
+
+        transform.rotation = rot;
+    }
+
+    public bool CanMove(Direction dir)
+    {
+        return false;
+    }
+}
---
+++ Assets/scripts/Tile.cs
@@ -0,0 +1,54 @@
+﻿public class Tile
+{
+    public bool left;
+    public bool right;
+    public bool top;
+    public bool bottom;
+
+    public static string CleanName(string name)
+    {
+        name = name.Replace(" ", string.Empty);
+
+        var index = name.IndexOf('(');
+
+        if (index > 0)
+        {
+            name = name.Substring(0, index);
+        }
+
+        return name;
+    }
+    public static Tile FromName(string name)
+    {
+        name = CleanName(name);
+
+        var tile = new Tile();
+
+        var walls = name.Split('_');
+
+        foreach (var wall in walls)
+        {
+            if (wall == "top")
+            {
+                tile.top = true;
+            }
+
+            if (wall == "bottom")
+            {
+                tile.bottom = true;
+            }
+
+            if (wall == "left")
+            {
+                tile.left = true;
+            }
+
+            if (wall == "right")
+            {
+                tile.right = true;
+            }
+        }
+
+        return tile;
+    }
+}
