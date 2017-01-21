2f86380d6291919e02baad4bd4aae56e50c991a8
tiagosomda <tsomda@gmail.com>
2017-01-21 06:22:48 -0800

    made movement animation a bit smoother

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -2,80 +2,120 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Direction { up, down, left, right };
+public enum Direction { up, down, left, right, none };
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Header("Control Keys")]
     public KeyCode moveLeft;
     public KeyCode moveRight;
     public KeyCode moveUp;
     public KeyCode moveDown;
 
-    public float speed;
+    [Header("Move Settings")]
+    [Range(0f, 1f)]
+    public float moveSpeed;
+    [Range(0f, 1f)]
+    public float rotationSpeed;
 
+    // does not show on inspector
     public Map map;
+
+    private Vector3 nextPos;
+    private Quaternion nextRot;
+
+    private Direction moveDir;
+    private float moveTimer;
+    private float rotationTimer;
 
 
     // Use this for initialization
     void Start () {
-        
-	}
+        moveDir = Direction.none;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetKeyUp(moveLeft))
+        if(moveDir != Direction.none)
+        {
+            return;
+        }
+
+        if(Input.GetKey(moveLeft))
         {
             Move(Direction.left);
         }
 
-        if (Input.GetKeyUp(moveRight))
+        if (Input.GetKey(moveRight))
         {
             Move(Direction.right);
         }
 
-        if (Input.GetKeyUp(moveUp))
+        if (Input.GetKey(moveUp))
         {
             Move(Direction.up);
         }
 
-        if (Input.GetKeyUp(moveDown))
+        if (Input.GetKey(moveDown))
         {
             Move(Direction.down);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        if(moveTimer > 0f)
+        {
+            var pos = Vector3.Lerp(nextPos, transform.position, moveTimer);
+            transform.position = pos;
+            moveTimer -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            moveDir = Direction.none;
+        }
+
+        if(rotationTimer > 0f)
+        {
+            var rot = Quaternion.Lerp(nextRot, transform.rotation, rotationTimer);
+            transform.rotation = rot;
+            rotationTimer -= Time.fixedDeltaTime;
         }
     }
 
     public void Move(Direction dir)
     {
         var pos = transform.position;
-        var rot = transform.rotation;
-
+        nextPos = pos;
         switch (dir)
         {
             case Direction.up:
-                pos.y += 1;
-                rot = Quaternion.Euler(0, 0, 0);
+                nextPos.y += 1;
+                nextRot = Quaternion.Euler(0, 0, 0);
                 break;
             case Direction.down:
-                pos.y -= 1;
-                rot = Quaternion.Euler(0, 0, 180);
+                nextPos.y -= 1;
+                nextRot = Quaternion.Euler(0, 0, 180);
                 break;
             case Direction.left:
-                rot = Quaternion.Euler(0, 0, 90);
-                pos.x -= 1;
+                nextPos.x -= 1;
+                nextRot = Quaternion.Euler(0, 0, 90);
                 break;
             case Direction.right:
-                pos.x += 1;
-                rot = Quaternion.Euler(0, 0, 270);
+                nextPos.x += 1;
+                nextRot = Quaternion.Euler(0, 0, 270);
                 break;
         }
 
         if (map.CanMove(transform.position, dir))
         {
-            transform.position = pos;
+            moveTimer = moveSpeed;
         }
 
-        transform.rotation = rot;
+        rotationTimer = rotationSpeed;
+
+        moveDir = dir;
     }
 
     public bool CanMove(Direction dir)
