95dfe88519319573534848e1b4c6da2f9fad3226
Kennedy Brown <kennedy.c.brown@gmail.com>
2017-01-22 03:29:57 -0800

    Basic 3D project

---
+++ Assets/SeekerBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SeekerBehavior : MonoBehaviour {
+
+	public Transform target;
+	public float proximity;
+	private NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent<NavMeshAgent> ();
+
+		nav.speed = 3f;
+		nav.SetDestination (target.position);
+
+		proximity = 2f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		var d = Vector3.Distance (transform.position, target.position);
+		//Debug.Log (d);
+
+		if (d < proximity)
+			nav.Stop ();
+	}
+}
